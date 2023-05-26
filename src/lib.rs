94fa76a4352f216e79029706977221673676f906
Eievui <14899090+eievui5@users.noreply.github.com>
2023-05-26 16:30:49 -0400

    Add non-cli interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #![feature(path_file_prefix)]
 
 pub use clap::Parser;
-use image::GenericImageView;
+use image::{GenericImageView, Rgba};
 use std::cmp::max;
 use std::ffi::OsStr;
 use std::fmt::Write;
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::exit;
 
 // This program is a heavily modified version of `makefont` from Esprit.
@@ -29,10 +29,6 @@ pub struct Cli {
 	#[clap(long, default_value="8")]
 	pub height: u32,
 
-	/// Width of the space character
-	#[clap(long, default_value="4")]
-	pub space: u32,
-
 	/// The first character in the font.
 	#[clap(long, default_value=" ")]
 	pub first: char,
@@ -48,75 +44,17 @@ impl Cli {
 			exit(1);
 		});
 
-		if img.width() % self.width != 0 {
-			eprintln!("Image width must be a multiple of {}", self.width);
-			exit(1);
-		}
+		let bm_location = self.bitmap_location.as_ref().unwrap_or(&self.input);
 
-		if img.height() % self.height != 0 {
-			eprintln!("Image width must be a multiple of {}", self.height);
-			exit(1);
-		}
-
-		let mut glyph = ' ' as u8;
-
-		let tile_count = (img.width() / self.width) * (img.height() / self.height);
-
-		let input_name = self.input.file_prefix().unwrap_or_else(|| {
-			eprintln!("Warning: {}'s `file` field (line 3) will need to be adjusted.", self.output.display());
-			OsStr::new("font")
-		});
-
-		let mut output = String::new();
-
-		writeln!(
-			output,
-			"info face=\"{}\" size={} bold=0 italic=0 unicode=1 stretchH=100 smooth=1 aa=0 padding=0,0,0,0 spacing=0,0",
-			input_name.to_string_lossy(),
-			self.width
-		).unwrap();
-		writeln!(
-			output,
-			"common lineHeight={} base=0 scaleW={} scaleH={} pages=1 packed=0",
+		let output = make_fnt(
+			img,
+			&self.input,
+			&self.output.to_string_lossy(),
+			self.width,
 			self.height,
-			img.width(),
-			img.height(),
-		).unwrap();
-		writeln!(
-			output,
-			"page id=0 file=\"{}\"",
-			self.bitmap_location.unwrap_or(self.input).display()
-		).unwrap();
-		writeln!(output, "chars count={tile_count}").unwrap();
-
-		for ty in (0..img.height()).step_by(self.height as usize) {
-			for tx in (0..img.width()).step_by(self.width as usize) {
-				let mut size = 0;
-
-				for y in ty..(ty + self.height) {
-					let mut row_size = 0;
-
-					for x in tx..(tx + self.width) {
-						let pixel = img.get_pixel(x, y);
-
-						if pixel.0[3] != 0 {
-							row_size = x - tx;
-						}
-					}
-
-					size = max(size, row_size);
-				}
-				size += 2;
-				writeln!(
-					output,
-					"char id={glyph} x={tx} y={ty} width={size} height=8 xoffset=0 yoffset=0 xadvance={size} page=0 chnl=0"
-				).unwrap();
-
-				glyph += 1;
-			}
-		}
-
-		writeln!(output, "kernings count=0").unwrap();
+			self.first,
+			bm_location,
+		);
 
 		match fs::write(&self.output, output) {
 			Err(err) => {
@@ -126,4 +64,85 @@ impl Cli {
 			_ => {}
 		}
 	}
+}
+
+pub fn make_fnt(
+	img: impl GenericImageView<Pixel = Rgba<u8>>,
+	img_path: &Path,
+	output_name: &str,
+	width: u32,
+	height: u32,
+	first: char,
+	bitmap_location: &PathBuf,
+) -> String {
+	if img.width() % width != 0 {
+		eprintln!("Image width must be a multiple of {}", width);
+		exit(1);
+	}
+
+	if img.height() % height != 0 {
+		eprintln!("Image width must be a multiple of {}", height);
+		exit(1);
+	}
+
+	let mut glyph = first as u8;
+
+	let tile_count = (img.width() / width) * (img.height() / height);
+
+	let input_name = img_path.file_prefix().unwrap_or_else(|| {
+		eprintln!("Warning: {}'s `file` field (line 3) will need to be adjusted.", output_name);
+		OsStr::new("font")
+	});
+
+	let mut output = String::new();
+
+	writeln!(
+		output,
+		"info face=\"{}\" size={} bold=0 italic=0 unicode=1 stretchH=100 smooth=1 aa=0 padding=0,0,0,0 spacing=0,0",
+		input_name.to_string_lossy(),
+		width
+	).unwrap();
+	writeln!(
+		output,
+		"common lineHeight={} base=0 scaleW={} scaleH={} pages=1 packed=0",
+		height,
+		img.width(),
+		img.height(),
+	).unwrap();
+	writeln!(
+		output,
+		"page id=0 file=\"{}\"",
+		bitmap_location.display()
+	).unwrap();
+	writeln!(output, "chars count={tile_count}").unwrap();
+
+	for ty in (0..img.height()).step_by(height as usize) {
+		for tx in (0..img.width()).step_by(width as usize) {
+			let mut size = 0;
+
+			for y in ty..(ty + height) {
+				let mut row_size = 0;
+
+				for x in tx..(tx + width) {
+					let pixel = img.get_pixel(x, y);
+
+					if pixel.0[3] != 0 {
+						row_size = x - tx;
+					}
+				}
+
+				size = max(size, row_size);
+			}
+			size += 2;
+			writeln!(
+				output,
+				"char id={glyph} x={tx} y={ty} width={size} height=8 xoffset=0 yoffset=0 xadvance={size} page=0 chnl=0"
+			).unwrap();
+
+			glyph += 1;
+		}
+	}
+
+	writeln!(output, "kernings count=0").unwrap();
+	output
 }
